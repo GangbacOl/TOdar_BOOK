146074b1437aea293638c894250480055c93bc61
GangbacOl <gangyeonjo336@gmail.com>
2020-06-20 20:59:49 +0900

    [UPD] POST /read 라우팅 작성.

--- back-end/routes/books/index.js
+++ back-end/routes/books/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./book.controller');
 
+router.post('/read', controller.readBooks);
 router.post('/add', controller.addBook);
 
 module.exports = router;
