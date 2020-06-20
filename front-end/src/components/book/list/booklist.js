4a12d1f5ee6b2dee64ec7db2a240d63ee05ea81b
GangbacOl <gangyeonjo336@gmail.com>
2020-06-20 21:04:36 +0900

    [UPD] BookList 컴포넌트 업데이트 1. useEffect를 사용하여 서버에서 저장된 책 정보들을 요청함. 2. Cors 에러가 생김

--- front-end/src/components/book/list/booklist.js
+++ front-end/src/components/book/list/booklist.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function Booklist() {
-    return (
-        <div>booklist</div>
-    )
-}
+const BookList = ({ username }) => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    axios.post("http://localhost:5000/books/read", { username }).then((res) => setBooks(res.data.response));
+  }, [username]);
+  console.log(books[0]);
+  const searchBookByIsbn = (isbn) => {
+    axios
+      .get(`https://openapi.naver.com/v1/search/book_adv.xml?d_isbn=${isbn}`, {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "X-Naver-Client-Id": "iB8LdjGuHwSuDU_5ZR6Q",
+          "X-Naver-Client-Secret": "Bno7XltwqA",
+        },
+      })
+      .then((res) => console.log(res));
+  };
+  // axios.get(`https://openapi.naver.com/v1/search/book_adv.xml?d_isbn=${books[0].isbn}`).then((res) => console.log(res));
+  return (
+    <div className="BookList">
+      <ul>
+        {books.map((book) => (
+          <div key={book.id}>
+            <h2>{searchBookByIsbn(book.isbn)}</h2>
+            <span>{book.amount_read}</span>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Booklist
+export default BookList;
