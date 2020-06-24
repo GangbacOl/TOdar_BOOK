9dbc9d9e816a2a4794360852313c494fd0e73d19
GangbacOl <gangyeonjo336@gmail.com>
2020-06-24 13:30:24 +0900

    [UPD] api 요청 비동기 처리(백엔드)

--- back-end/routes/books/book.controller.js
+++ back-end/routes/books/book.controller.js
@@ -1,6 +1,39 @@
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 const models = require('../../models');
 const authMiddleware = require('../../middlewares/auth/auth');
+
+const searchBookByIsbn = (item) => {
+    return axios.get(`https://openapi.naver.com/v1/search/book_adv.json?d_isbn=${item.isbn}`, {
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'X-Naver-Client-Id': 'iB8LdjGuHwSuDU_5ZR6Q',
+            'X-Naver-Client-Secret': 'Bno7XltwqA',
+        },
+    });
+};
+
+exports.readBooks = async (req, res) => {
+    const { username } = req.body;
+    let response = await models.users_books.findAll({
+        where: { username },
+    });
+    const booksInfo = await new Promise(async (resolve, reject) => {
+        let booksInfo = { items: [] };
+        let data = await Promise.all(
+            response.map((item) => {
+                return searchBookByIsbn(item);
+            })
+        );
+        data.forEach((book) => {
+            booksInfo.items.push(book.data.items[0]);
+        });
+        resolve(booksInfo);
+    });
+    res.json({
+        booksInfo,
+    });
+};
 
 exports.addBook = (req, res) => {
     const { isbn, username, percentage } = req.body;
@@ -26,17 +59,4 @@ exports.addBook = (req, res) => {
                 });
         }
     }
-};
-
-exports.readBooks = (req, res) => {
-    const { username } = req.body;
-    models.users_books
-        .findAll({
-            where: { username },
-        })
-        .then((response) => {
-            res.json({
-                response,
-            });
-        });
 };
