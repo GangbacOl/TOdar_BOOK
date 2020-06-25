60709dc2697113fe3864357cd73348515d8430e8
GangbacOl <gangyeonjo336@gmail.com>
2020-06-25 23:55:34 +0900

    [UPD] 로그인 유무로 흐름 제어 추가.

--- front-end/src/components/book/list/booklist.js
+++ front-end/src/components/book/list/booklist.js
@@ -1,31 +1,54 @@
 import React, { useState, useEffect } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
 import axios from "axios";
 
 const BookList = ({ username }) => {
   const [books, setBooks] = useState([]);
+  const [errorContent, setErrorContent] = useState({});
+  const [isLogin, setIsLogin] = useState(false);
   useEffect(() => {
-    axios.post("http://localhost:5000/books/read", { username }).then((res) => setBooks(res.data.booksInfo.items));
+    axios
+      .post("http://localhost:5000/books/read", { username }, { withCredentials: true })
+      .then((res) => {
+        setBooks(res.data.booksInfo.items);
+        setIsLogin(true);
+      })
+      .catch(({ response }) => {
+        setErrorContent({ statusText: response.statusText, errorMessage: response.data.message });
+        if (response.status === 401) {
+          setIsLogin(false);
+        }
+      });
   }, [username]);
-  console.log(books);
   return (
     <div className="BookList">
-      <ul>
-        {books.map((book) => (
-          <div key={book.isbn}>
-            <h2>{book.title}</h2>
-            <span>
-              저자: <b>{book.author}</b>
-            </span>
-            <br />
-            <span>
-              {" "}
-              출판사: <b>{book.publisher}</b>
-            </span>
-            <br />
-            <img src={book.image} alt="" />
-          </div>
-        ))}
-      </ul>
+      {isLogin ? (
+        <div>
+          {books.map((book) => (
+            <div key={book.isbn}>
+              <h2>{book.title}</h2>
+              <span>
+                저자: <b>{book.author}</b>
+              </span>
+              <br />
+              <span>
+                {" "}
+                출판사: <b>{book.publisher}</b>
+              </span>
+              <br />
+              <ProgressBar now={book.amount_read} label={`${book.amount_read}%`} />
+              <br />
+              <img src={book.image} alt="" />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h2>{errorContent.statusText}</h2>
+          <p>{errorContent.errorMessage}</p>
+          <a href="/signin">로그인하러 가기</a>
+        </div>
+      )}
     </div>
   );
 };
