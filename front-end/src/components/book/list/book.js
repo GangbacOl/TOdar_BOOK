eab4a0c9fe29a2650f1e3b16cbd6e8ea36eb9225
GangbacOl <gangyeonjo336@gmail.com>
2020-06-26 23:10:50 +0900

    [ADD] Book 컴포넌트 생성.

---
+++ front-end/src/components/book/list/book.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+const Book = ({ isbn, title, author, publisher, percentage, image }) => {
+  return (
+    <div key={isbn}>
+      <h2>{title}</h2>
+      <span>
+        저자: <b>{author}</b>
+      </span>
+      <br />
+      <span>
+        출판사: <b>{publisher}</b>
+      </span>
+      <br />
+      <ProgressBar now={percentage} label={`${percentage}%`} />
+      <br />
+      <img src={image} alt="" />
+    </div>
+  );
+};
+
+export default Book;
