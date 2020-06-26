492ecadf256d85cd7309288eca239baf5f55b89c
GangbacOl <gangyeonjo336@gmail.com>
2020-06-26 23:11:57 +0900

    [ADD] ErrorPage 컴포넌트 생성.

---
+++ front-end/src/components/error/error.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ErrorPage = ({ statusText, errorMessage, redirectUrl, redirectMessage }) => {
+  return (
+    <div className="ErrorPage">
+      <h2>{statusText}</h2>
+      <p>{errorMessage}</p>
+      <a href={redirectUrl}>{redirectMessage}</a>
+    </div>
+  );
+};
+
+export default ErrorPage;
