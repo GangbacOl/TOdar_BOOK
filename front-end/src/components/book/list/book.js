eb81bd2fa4b64700cfdeb8f1bb07f639e4afc84a
GangbacOl <gangyeonjo336@gmail.com>
2020-06-27 23:52:09 +0900

    [UPD] 코드 수정.

--- front-end/src/components/book/list/book.js
+++ front-end/src/components/book/list/book.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ProgressBar from "react-bootstrap/ProgressBar";
+import { ProgressBar } from "react-bootstrap";
 
 const Book = ({ isbn, title, author, publisher, percentage, image }) => {
   return (
