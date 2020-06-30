c4de153251df9476e8922ab514432dd9fab63115
GangbacOl <gangyeonjo336@gmail.com>
2020-06-30 23:57:30 +0900

    [UPD]

--- back-end/middlewares/auth/auth.js
+++ back-end/middlewares/auth/auth.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res) => {
     const token = req.cookies.user;
     const secret = req.app.get('jwt-secret');
-    console.log('awefewf');
 
     if (!token) {
         res.status(401).json({
