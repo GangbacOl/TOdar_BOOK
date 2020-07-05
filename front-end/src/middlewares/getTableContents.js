f47ff1d24298b015a82f52cf09cdd14b0870225f
GangbacOl <gangyeonjo336@gmail.com>
2020-07-06 01:33:56 +0900

    [UPD] 목차 구조 업데이트 및 프로퍼티 이름 변경.

--- front-end/src/middlewares/getTableContents.js
+++ front-end/src/middlewares/getTableContents.js
@@ -1,65 +1,67 @@
 const getTableOfContents = () => {
   return {
-    1: {
-      name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      type: "h1",
-      check: false,
-    },
-    2: {
-      name: "In congue dui in ante volutpat, ut faucibus nibh sollicitudin.",
-      type: "h1",
-      check: false,
-    },
-    3: {
-      name: "Maecenas tempus massa non leo feugiat, quis porta felis consectetur.",
-      type: "h1",
-      check: false,
-    },
-    4: {
-      name: "Etiam quis elit vestibulum, accumsan sapien id, consequat neque.",
-      type: "h1",
-      check: false,
-    },
-    5: {
-      name: "Pellentesque ultrices erat eu purus rhoncus dignissim.",
-      type: "h1",
-      check: false,
-    },
-    6: {
-      name: "Integer efficitur eros ut mi consectetur rutrum.",
-      type: "h1",
-      check: false,
-    },
-    7: {
-      name: "Nulla cursus dolor eget pulvinar vulputate.",
-      type: "h1",
-      check: false,
-    },
-    8: {
-      name: "Vivamus pellentesque ligula in sem porta fermentum blandit sit amet ante.",
-      type: "h1",
-      check: false,
-    },
-    9: {
-      name: "Praesent eu purus vitae nunc vulputate dapibus.",
-      type: "h1",
-      check: false,
-    },
-    10: {
-      name: "Donec ultrices tellus quis rutrum luctus.",
-      type: "h1",
-      check: false,
-    },
-    11: {
-      name: "Nullam eget nunc eget elit sagittis blandit.",
-      type: "h1",
-      check: false,
-    },
-    12: {
-      name: "Donec mollis lectus vel est molestie consequat.",
-      type: "h1",
-      check: false,
-    },
+    contents: [
+      {
+        name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "In congue dui in ante volutpat, ut faucibus nibh sollicitudin.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "Maecenas tempus massa non leo feugiat, quis porta felis consectetur.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "Etiam quis elit vestibulum, accumsan sapien id, consequat neque.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "Pellentesque ultrices erat eu purus rhoncus dignissim.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "Integer efficitur eros ut mi consectetur rutrum.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "Nulla cursus dolor eget pulvinar vulputate.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "Vivamus pellentesque ligula in sem porta fermentum blandit sit amet ante.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "Praesent eu purus vitae nunc vulputate dapibus.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "Donec ultrices tellus quis rutrum luctus.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "Nullam eget nunc eget elit sagittis blandit.",
+        type: "h1",
+        checked: false,
+      },
+      {
+        name: "Donec mollis lectus vel est molestie consequat.",
+        type: "h1",
+        checked: false,
+      },
+    ],
   };
 };
 
