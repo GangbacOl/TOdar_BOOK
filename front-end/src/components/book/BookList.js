fd117efbc537a413f074161a9575948bcfdd9769
GangbacOl <gangyeonjo336@gmail.com>
2020-07-12 23:46:06 +0900

    [UPD] 1. post -> get 2. 코드 정리.

--- front-end/src/components/book/BookList.js
+++ front-end/src/components/book/BookList.js
@@ -4,30 +4,23 @@ import Loader from "react-loader-spinner";
 import { useHistory } from "react-router";
 import ErrorPage from "../error/ErrorPage";
 import Book from "./Book";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-const BookList = ({ username }) => {
+const BookList = ({ username, isLogin }) => {
     const [books, setBooks] = useState([]);
     const [errorContent, setErrorContent] = useState({});
-    const [isLogin, setIsLogin] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const history = useHistory();
 
-    const goHome = () => {
-        history.push("/");
-    };
     useEffect(() => {
         axios
-            .post("http://localhost:5000/books/read", { username }, { withCredentials: true })
+            .get("http://localhost:5000/books/read", { params: { username }, withCredentials: true })
             .then((res) => {
                 setBooks(res.data.booksRead);
-                setIsLogin(true);
                 setIsLoading(false);
             })
             .catch(({ response }) => {
                 if (response.status === 401) {
                     setErrorContent({ statusText: response.statusText, errorMessage: response.data.message });
-                    setIsLogin(false);
                     setIsLoading(false);
                 }
             });
@@ -38,20 +31,14 @@ const BookList = ({ username }) => {
                 <Loader type="Oval" color="#000000" height="50" width="50" />
             ) : isLogin ? (
                 <div>
-                    <input
-                        type="button"
-                        onClick={() => {
-                            goHome();
-                        }}
-                        value="메인으로"
-                    />
+                    <input type="button" onClick={() => history.push("/")} value="메인으로" />
                     {books.map((book) => (
                         <Book
                             isbn={book.isbn}
                             title={book.title}
                             author={book.author}
                             publisher={book.publisher}
-                            percentage={book.amount_read}
+                            percentage={book.percentage}
                             image={book.image}
                             tableOfContents={book.tableOfContents}
                             key={book.isbn}
