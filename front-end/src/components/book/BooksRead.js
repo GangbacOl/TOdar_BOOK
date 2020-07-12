a1dc43daf363a2a96cbcf3782ef085ff50ec3eab
GangbacOl <gangyeonjo336@gmail.com>
2020-07-12 23:47:45 +0900

    [UPD] 1. ErrorPage 컴포넌트 사용. 2. 로그인 유무에 따른 흐름 제어.

--- front-end/src/components/book/BooksRead.js
+++ front-end/src/components/book/BooksRead.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Loader from "react-loader-spinner";
+import { useHistory } from "react-router";
 import BookRead from "./BookRead";
+import ErrorPage from "../error/ErrorPage";
 
-const BooksRead = ({ username }) => {
+const BooksRead = ({ username, isLogin }) => {
     const [books, setBooks] = useState([]);
+    const [errorContent, setErrorContent] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+    const history = useHistory();
 
     useEffect(() => {
         axios
@@ -18,18 +22,34 @@ const BooksRead = ({ username }) => {
             .then((res) => {
                 setBooks(res.data.booksRead);
                 setIsLoading(false);
+            })
+            .catch(({ response }) => {
+                if (response.status === 401) {
+                    setErrorContent({ statusText: response.statusText, errorMessage: response.data.message });
+                    setIsLoading(false);
+                }
             });
     }, [username]);
+
     return (
-        <div>
+        <div className="BooksRead">
             {isLoading ? (
                 <Loader type="Oval" color="#000000" height="50" width="50" />
+            ) : isLogin ? (
+                <div>
+                    <input type="button" onClick={() => history.push("/")} value="메인으로" />
+                    {books.map((book) => {
+                        return <BookRead thumbnail={book.image} author={book.author} title={book.title} key={book.isbn} />;
+                    })}
+                </div>
             ) : (
-                books.map((book) => {
-                    return <BookRead thumbnail={book.image} author={book.author} title={book.title} key={book.isbn} />;
-                })
+                <ErrorPage
+                    statusText={errorContent.statusText}
+                    errorMessage={errorContent.errorMessage}
+                    redirectUrl="/signin"
+                    redirectMessage="로그인하러 가기"
+                />
             )}
-            }
         </div>
     );
 };
