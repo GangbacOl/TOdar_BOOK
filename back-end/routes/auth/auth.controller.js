751d7dce6c540a25f06ce2bc5edcb67fc6b24dfa
GangbacOl <gangyeonjo336@gmail.com>
2020-07-12 23:33:24 +0900

    [UPD] 코드 최적화.

--- back-end/routes/auth/auth.controller.js
+++ back-end/routes/auth/auth.controller.js
@@ -6,9 +6,8 @@ const config = require('../config');
 exports.signup = (req, res) => {
     const { id, password, username } = req.body;
     if (!id || !password || !username) {
-        res.render('auth/error', {
+        res.status(400).json({
             message: '회원가입 실패(회원 정보 미기입)',
-            type: 'signup',
         });
     } else {
         const encrypted = crypto.createHmac('sha1', config.secret).update(password).digest('base64');
@@ -18,18 +17,14 @@ exports.signup = (req, res) => {
                 password: encrypted,
                 username: username,
             })
-            .then((result) => {
-                console.log('데이터 추가 완료: ' + result);
+            .then((response) => {
                 res.status(200).json({
-                    success: true,
-                    redirectUrl: '/signin',
+                    message: '회원가입 성공',
                 });
             })
             .catch((err) => {
-                console.log('데이터 추가 실패' + err);
-                res.render('auth/error', {
+                res.status(401).json({
                     message: '회원가입 실패',
-                    type: 'signup',
                 });
             });
     }
@@ -39,31 +34,28 @@ exports.signin = (req, res) => {
     const { id, password } = req.body;
 
     const encrypted = crypto.createHmac('sha1', config.secret).update(password).digest('base64');
-
     models.user
         .findOne({
             where: { account: id },
         })
-        .then((userInfo) => {
+        .then((response) => {
             // 로그인 실패 시
-            if (userInfo === null)
-                res.render('auth/error', {
-                    message: '로그인 실패',
-                    type: 'signin',
+            if (response === null)
+                res.status(401).json({
+                    message: '로그인 실패(존재하지 않는 계정)',
                 });
             // 패스워드 오류 시
-            else if (userInfo.dataValues.password !== encrypted)
-                res.render('auth/error', {
+            else if (response.dataValues.password !== encrypted)
+                res.status(401).json({
                     message: '패스워드가 일치하지 않음',
-                    type: 'signin',
                 });
             // 로그인 성공 시
-            else if (userInfo.dataValues.password === encrypted) {
+            else if (response.dataValues.password === encrypted) {
                 console.log('로그인 성공');
                 const token = jwt.sign(
                     {
                         _id: id,
-                        username: userInfo.dataValues.username,
+                        username: response.dataValues.username,
                     },
                     config.secret,
                     {
@@ -79,10 +71,9 @@ exports.signin = (req, res) => {
                     })
                     .json({
                         userData: {
-                            username: userInfo.dataValues.username,
+                            username: response.dataValues.username,
                         },
-                        message: 'success',
-                        redirectUrl: '/',
+                        message: '로그인 성공',
                     });
             }
         });
