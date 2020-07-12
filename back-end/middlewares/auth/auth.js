74705b574b575cffd4392bedaf48ca62ee552fd4
GangbacOl <gangyeonjo336@gmail.com>
2020-07-12 23:32:31 +0900

    [UPD] 코드 수정.

--- back-end/middlewares/auth/auth.js
+++ back-end/middlewares/auth/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res) => {
     const token = req.cookies.user;
-    console.log(token);
+    console.log('token:' + token);
     if (!token) {
         res.status(401).json({
             message: '로그인을 하세요.',
