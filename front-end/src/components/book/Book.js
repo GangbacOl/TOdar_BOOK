0a7780836c154e460c1998ed3362f8e657bdc7e3
GangbacOl <gangyeonjo336@gmail.com>
2020-07-18 23:57:33 +0900

    [UPD] 1. ReadingBookModal 한 파일내에 컴포넌트 생성. 2. react-bootstrap의 Modal 컴포넌트를 사용해서 ReadingBookModal 컴포넌트를 모달로 띄움.

--- front-end/src/components/book/Book.js
+++ front-end/src/components/book/Book.js
@@ -1,13 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ProgressBar } from "react-bootstrap";
-import ReadingBook from "./ReadingBook";
+import { Button } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
+// import ReadingBook from "./ReadingBook";
 import "./style/Book.scss";
+import "./style/ReadingBook.scss";
 
-const Book = ({ isbn, title, author, contents, publisher, percentage, image, tableOfContents, username }) => {
-    const [showTableContents, setShowTableContents] = useState(false);
+const ReadingBookModal = ({ showModal, handleClose, tableOfContents, title, isbn, username }) => {
+    useEffect(() => {
+        readCheckingToggle(tableOfContents);
+    });
+    const readCheckingToggle = (tableOfContents) => {
+        const checkList = document.getElementsByClassName("isRead");
+        tableOfContents.contents.map((content, index) => {
+            checkList.item(index).checked = content.checked;
+        });
+    };
 
-    const showContents = () => setShowTableContents(!showTableContents);
+    const sendData = async (tableOfContents) => {
+        let newTableOfContents = tableOfContents;
+        console.log(tableOfContents);
+        const checkList = document.getElementsByClassName("isRead");
+
+        newTableOfContents.contents.map((content, index) => {
+            content.checked = checkList.item(index).checked;
+        });
+
+        const percentage = setReadPercentage(newTableOfContents);
+        await axios
+            .post("http://localhost:5000/books/update", { newTableOfContents, percentage, isbn, username }, { withCredentials: true })
+            .catch((err) => console.log(err));
+        window.location.reload();
+    };
+
+    const setReadPercentage = (newTableOfContents) => {
+        let tempPercentage = 0,
+            tempCount = 0;
+        newTableOfContents.contents.map((content) => {
+            if (content.checked === true) tempPercentage++;
+            tempCount++;
+        });
+        return (tempPercentage / tempCount) * 100;
+    };
 
     const cancelRead = async (isbn, username) => {
         await axios
@@ -15,14 +50,43 @@ const Book = ({ isbn, title, author, contents, publisher, percentage, image, tab
             .catch((err) => console.log(err));
         window.location.reload();
     };
+    return (
+        <Modal show={showModal} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {tableOfContents.contents.map((item, index) => (
+                    <div key={index}>
+                        <li>{item.name}</li>
+                        <input className="isRead" type="checkbox" />
+                    </div>
+                ))}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => cancelRead(isbn, username)}>
+                    책 삭제
+                </Button>
+                <Button variant="primary" onClick={() => sendData(tableOfContents)}>
+                    업데이트
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
 
+const Book = ({ isbn, title, author, contents, publisher, percentage, image, tableOfContents, username }) => {
+    const [showModal, setShowModal] = useState(false);
+
+    console.log("start: " + showModal);
     const sliceTitle = (title) => {
         let newTitle = title.split("(");
         return newTitle[0];
     };
+
     return (
         <div className="Book">
-            <div className="book-container">
+            <div className="book-container" onClick={() => setShowModal(true)}>
                 <h3>{sliceTitle(title)}</h3>
                 <div className="main-contents">
                     <img src={image} alt="" />
@@ -38,22 +102,17 @@ const Book = ({ isbn, title, author, contents, publisher, percentage, image, tab
                     </div>
                 </div>
                 <ProgressBar now={percentage} />
-                {showTableContents ? (
-                    <input type="button" value="닫기" onClick={showContents} />
-                ) : (
-                    <div className="button-container">
-                        <input type="button" value="체크하러 가기" onClick={showContents} />
-                        <input
-                            type="button"
-                            value="책 삭제하기"
-                            onClick={() => {
-                                cancelRead(isbn, username);
-                            }}
-                        />
-                    </div>
-                )}
             </div>
-            {showTableContents ? <ReadingBook tableOfContents={tableOfContents} isbn={isbn} username={username} /> : null}
+            {showModal ? (
+                <ReadingBookModal
+                    showModal={showModal}
+                    handleClose={() => setShowModal(false)}
+                    tableOfContents={tableOfContents}
+                    title={title}
+                    isbn={isbn}
+                    username={username}
+                />
+            ) : null}
         </div>
     );
 };
