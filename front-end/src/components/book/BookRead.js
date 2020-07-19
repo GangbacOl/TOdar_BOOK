79935db129354dc0ca1820d2043c9e970bcded43
GangbacOl <gangyeonjo336@gmail.com>
2020-07-19 22:01:07 +0900

    [UPD] 1. 독서 시작, 끝 날짜 출력. 2. 제목의 불필요한 부분을 잘라주는 sliceTitle 메소드 생성. 3. 스타일시트 적용.

--- front-end/src/components/book/BookRead.js
+++ front-end/src/components/book/BookRead.js
@@ -1,11 +1,26 @@
 import React from "react";
+import "./style/BookRead.scss";
 
-const BookRead = ({ thumbnail, author, title }) => {
+const BookRead = ({ thumbnail, author, title, startOfRead, finishOfRead }) => {
+    const sliceTitle = (title) => {
+        let newTitle = title.split("(");
+        return newTitle[0];
+    };
     return (
         <div className="BookRead">
-            <h2>{title}</h2>
-            <span>{author}</span>
+            <h3>{sliceTitle(title)}</h3>
             <img src={thumbnail} alt="" />
+            <span>저자: {author}</span>
+            <div className="date">
+                <div>
+                    <span>읽기 시작한 날짜 </span>
+                    <span>{startOfRead.slice(0, 10) + " " + startOfRead.slice(11, 19)}</span>
+                </div>
+                <div>
+                    <span>다 읽은 날짜 </span>
+                    <span>{finishOfRead.slice(0, 10) + " " + finishOfRead.slice(11, 19)}</span>
+                </div>{" "}
+            </div>
         </div>
     );
 };
