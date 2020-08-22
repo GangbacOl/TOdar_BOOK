64b9594b86510d37519a6b86bd37b77a5dde840b
GangbacOl <gangyeonjo336@gmail.com>
2020-08-23 02:30:40 +0900

    [UPD] 1. common 디렉토리로 이동. 2. Link 컴포넌트 사용. 3. className 변경.

--- front-end/src/components/common/Header.js
+++ front-end/src/components/common/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import cookie from "react-cookies";
 import "./style/header.scss";
 
@@ -10,18 +11,20 @@ const Header = ({ setIsLogin, isLogin, initializeState, username }) => {
         window.location.reload();
     };
     return (
-        <div className="header">
+        <div className="Header">
             <h1>TODAR BOOK</h1>
             <div>
-                <a href="/book">내 책</a>
-                <a href="/booksRead">내가 읽은 책</a>
                 {isLogin ? (
                     <div>
-                        <span>{username}님, 안녕하세요</span>
-                        <input type="button" value="로그아웃" onClick={() => logout()} />
+                        <Link to="/book">내 책</Link>
+                        <Link to="/booksread">내 정보</Link>
+                        <div>
+                            <span>{username}님, 안녕하세요</span>
+                            <input type="button" value="로그아웃" onClick={() => logout()} />
+                        </div>
                     </div>
                 ) : (
-                    <a href="/signin">로그인 / 회원가입</a>
+                    <Link to="/login">로그인 / 회원가입</Link>
                 )}
             </div>
         </div>
