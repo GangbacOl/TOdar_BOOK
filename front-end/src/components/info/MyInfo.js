bea6a9bab2a8431dd2638f0f947dd4654b1af4dc
GangbacOl <gangyeonjo336@gmail.com>
2020-08-24 21:47:38 +0900

    [UPD] 읽은 책의 권수를 조회하는 api에 post 요청.

--- front-end/src/components/info/MyInfo.js
+++ front-end/src/components/info/MyInfo.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const MyInfo = ({ username }) => {
+    const [numberOfBooks, setNumberOfBooks] = useState(0);
+    useEffect(() => {
+        axios
+            .post("http://localhost:5000/books/countNmbrBooksRead", { username }, { withCredentials: true })
+            .then((res) => setNumberOfBooks(res.data.numberOfBooks));
+    });
     return (
         <div className="MyInfo">
             <h1>GangbacOl</h1>
             <hr />
             <span>
-                읽은 책: <b>10권</b>
+                읽은 책: <b>{numberOfBooks}권</b>
             </span>
             <span>
                 가입 일: <b>34일</b>
