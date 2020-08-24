6c319da91481625ed0b8584353c35cded0ebf79a
GangbacOl <gangyeonjo336@gmail.com>
2020-08-25 03:35:34 +0900

    [UPD] 요청 반환값 반영.

--- front-end/src/components/info/MyInfo.js
+++ front-end/src/components/info/MyInfo.js
@@ -2,21 +2,25 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const MyInfo = ({ username }) => {
-    const [numberOfBooks, setNumberOfBooks] = useState(0);
+    const [nmbrBooksRead, setNumberOfBooks] = useState(0);
+    const [nmbrJoinDays, setNmbrJoinDays] = useState(0);
     useEffect(() => {
         axios
             .post("http://localhost:5000/books/countNmbrBooksRead", { username }, { withCredentials: true })
-            .then((res) => setNumberOfBooks(res.data.numberOfBooks));
+            .then((res) => setNumberOfBooks(res.data.nmbrBooksRead));
+        axios
+            .post("http://localhost:5000/user/countNmbrJoinDays", { username }, { withCredentials: true })
+            .then((res) => setNmbrJoinDays(res.data.nmbrJoinDays));
     });
     return (
         <div className="MyInfo">
-            <h1>GangbacOl</h1>
+            <h1>{username}</h1>
             <hr />
             <span>
-                읽은 책: <b>{numberOfBooks}권</b>
+                읽은 책: <b>{nmbrBooksRead}권</b>
             </span>
             <span>
-                가입 일: <b>34일</b>
+                가입 일: <b>{nmbrJoinDays}일</b>
             </span>
         </div>
     );
