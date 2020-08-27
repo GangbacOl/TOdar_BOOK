d77fcf02e93989f044509f48c08ab8f45f62cfa1
GangbacOl <gangyeonjo336@gmail.com>
2020-08-28 01:54:32 +0900

    [UPD] 1. ErrorPage 삭제. 2. 로그인 하지 않은 경우 '/login'으로 강제 리다이렉트 시키는 로직 작성.

--- front-end/src/components/book/BookList.js
+++ front-end/src/components/book/BookList.js
@@ -1,60 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
 import axios from "axios";
-import ErrorPage from "../error/ErrorPage";
 import Book from "./Book";
 import "./style/BookList.scss";
 
 const BookList = ({ username, isLogin }) => {
     const [books, setBooks] = useState([]);
-    const [errorContent, setErrorContent] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+    const history = useHistory();
 
     useEffect(() => {
+        if (!isLogin) history.push("/login");
         axios
             .get("http://localhost:5000/books/read", { params: { username }, withCredentials: true })
             .then((res) => {
                 setBooks(res.data.booksRead);
                 setIsLoading(false);
             })
-            .catch(({ response }) => {
-                if (response.status === 401) {
-                    setErrorContent({ statusText: response.statusText, errorMessage: response.data.message });
-                    setIsLoading(false);
-                }
-            });
+            .catch((err) => console.log(err));
     }, [username]);
     return (
         <div className="BookList">
             {isLoading ? (
                 <p>Loading...</p>
-            ) : isLogin ? (
+            ) : (
                 <div className="booklist-container-first">
                     <Link to="/">메인으로</Link>
                     <div className="booklist-container-second">
-                        {books.map((book) => (
-                            <Book
-                                isbn={book.isbn}
-                                title={book.title}
-                                author={book.authors[0]}
-                                contents={book.contents}
-                                publisher={book.publisher}
-                                percentage={book.percentage}
-                                image={book.thumbnail}
-                                tableOfContents={book.tableOfContents}
-                                key={book.isbn}
-                                username={username}
-                            />
-                        ))}
+                        {books.length ? (
+                            books.map((book) => (
+                                <Book
+                                    isbn={book.isbn}
+                                    title={book.title}
+                                    author={book.authors[0]}
+                                    contents={book.contents}
+                                    publisher={book.publisher}
+                                    percentage={book.percentage}
+                                    image={book.thumbnail}
+                                    tableOfContents={book.tableOfContents}
+                                    key={book.isbn}
+                                    username={username}
+                                />
+                            ))
+                        ) : (
+                            <p>추가한 책이 없습니다.</p>
+                        )}
                     </div>
                 </div>
-            ) : (
-                <ErrorPage
-                    statusText={errorContent.statusText}
-                    errorMessage={errorContent.errorMessage}
-                    redirectUrl="/signin"
-                    redirectMessage="로그인하러 가기"
-                />
             )}
         </div>
     );
