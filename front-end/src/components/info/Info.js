1241148bdd657ca5fc355280790163a6d73f8b1a
GangbacOl <gangyeonjo336@gmail.com>
2020-08-28 01:57:38 +0900

    [UPD] 로그인 하지 않은 경우 '/login'으로 강제 리다이렉트 시키는 로직 작성.

--- front-end/src/components/info/Info.js
+++ front-end/src/components/info/Info.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
 import FinishBookList from "./FinishBookList";
 import MyInfo from "./MyInfo";
 import "./style/Info.scss";
 
 const Info = ({ username, isLogin }) => {
+    const history = useHistory();
+
     return (
         <div className="Info">
             <Link to="/">메인으로</Link>
-            <div className="InfoMain">
-                <MyInfo username={username} />
-                <FinishBookList username={username} />
-            </div>
+            {isLogin ? (
+                <div className="InfoMain">
+                    <MyInfo username={username} />
+                    <FinishBookList username={username} />
+                </div>
+            ) : (
+                history.push("/login")
+            )}
         </div>
     );
 };
