bc6b8fd8a9556a63ff18680fa602461eea683015
GangbacOl <gangyeonjo336@gmail.com>
2020-08-27 21:17:24 +0900

    [UPD] jsx 업데이트.

--- front-end/src/components/info/FinishBook.js
+++ front-end/src/components/info/FinishBook.js
@@ -10,14 +10,15 @@ const FinishBook = ({ thumbnail, author, title, startOfRead, finishOfRead }) => 
         <div className="FinishBook">
             <h3>{sliceTitle(title)}</h3>
             <img src={thumbnail} alt="" />
+            <br />
             <span>저자: {author}</span>
             <div className="date">
                 <div>
-                    <span>읽기 시작한 날짜 </span>
+                    <span>Started at: </span>
                     <span>{startOfRead.slice(0, 10) + " " + startOfRead.slice(11, 19)}</span>
                 </div>
                 <div>
-                    <span>다 읽은 날짜 </span>
+                    <span>Finished at: </span>
                     <span>{finishOfRead.slice(0, 10) + " " + finishOfRead.slice(11, 19)}</span>
                 </div>
             </div>
